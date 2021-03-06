96c20d89608caad11820ef55a66f7f65aaef96d9
Akul Singhal <singhal.akul9@gmail.com>
2021-03-06 22:18:27 +0530

    implement kmeans(not tested)

---
+++ kmeans.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+
+def kmeans_iterate(df, centroids):
+    n = df.shape[0]
+    k = len(centroids)
+    cluster_number = np.zeros(n, dtype=np.int)
+    cluster_members = [[] for i in range(k)]
+    for i in range(n):
+        distances = np.linalg.norm(centroids - df.iloc[i].to_numpy(), axis=1)
+        cluster_number[i] = int(np.argmin(distances))
+        # print(cluster_number[i])
+        cluster_members[cluster_number[i]].append(i)
+    new_centroids = centroids.copy()
+    for i in range(k):
+        new_centroids[i] = df.iloc[cluster_members[i]].to_numpy().mean(axis=0)
+    return new_centroids
+
+
+def kmeans(df, k):
+    n = df.shape[0]
+    if k > n or k < 0:
+        return -1
+    centroid_indices = np.random.choice(n, k, replace=False)
+    centroids = df.iloc[centroid_indices].to_numpy()
+    new_centroids = kmeans_iterate(df, centroids)
+    while not ((centroids == new_centroids).all()):
+        centroids = new_centroids
+        new_centroids = kmeans_iterate(df, centroids)
+
+    cluster_number = np.zeros(n, dtype=np.int)
+    for i in range(n):
+        distances = np.linalg.norm(centroids - df.iloc[i].to_numpy(), axis=1)
+        cluster_number[i] = int(np.argmin(distances))
+    return cluster_number, centroids
