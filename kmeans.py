90f41b08811d0b640985572ae753ab1cf9101057
Akul Singhal <singhal.akul9@gmail.com>
2021-03-07 21:16:28 +0530

    add maxiter parameter to kmeans

--- kmeans.py
+++ kmeans.py
@@ -10,7 +10,7 @@ def kmeans_iterate(df, centroids):
     cluster_members = [[] for i in range(k)]
     for i in range(n):
         distances = np.linalg.norm(centroids - df.iloc[i].to_numpy(), axis=1)
-        cluster_number[i] = int(np.argmin(distances))
+        cluster_number[i] = round(np.argmin(distances))
         # print(cluster_number[i])
         cluster_members[cluster_number[i]].append(i)
     new_centroids = centroids.copy()
@@ -19,19 +19,23 @@ def kmeans_iterate(df, centroids):
     return new_centroids
 
 
-def kmeans(df, k):
+def kmeans(df, k, maxiter=np.inf):
     n = df.shape[0]
     if k > n or k < 0:
         return -1
     centroid_indices = np.random.choice(n, k, replace=False)
     centroids = df.iloc[centroid_indices].to_numpy()
     new_centroids = kmeans_iterate(df, centroids)
+    iter = 0
     while not ((centroids == new_centroids).all()):
         centroids = new_centroids
         new_centroids = kmeans_iterate(df, centroids)
+        iter += 1
+        if iter >= maxiter:
+            break
 
     cluster_number = np.zeros(n, dtype=np.int)
     for i in range(n):
         distances = np.linalg.norm(centroids - df.iloc[i].to_numpy(), axis=1)
-        cluster_number[i] = int(np.argmin(distances))
+        cluster_number[i] = round(np.argmin(distances))
     return cluster_number, centroids
